32905d38f5b12272c11bd17dfad058c8697ebb74
Brandyn <hufferbrandyn@gmail.com>
2022-01-23 20:06:18 -0600

    fixes

--- server/server.js
+++ server/server.js
@@ -32,5 +32,8 @@ app.get('*', (req, res) => {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
 });
