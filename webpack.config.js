ed1356ba1df2483dd3612f0f47322d4b8fe091e6
Brandyn <hufferbrandyn@gmail.com>
2022-01-31 16:20:05 -0600

    PWA tweeks

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "/client/src/index.js",
+  entry: "./client/src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
