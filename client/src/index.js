c61678cac7575dc0f4c9f8c22be23d48b8b29a39
Brandyn <hufferbrandyn@gmail.com>
2022-01-31 17:21:52 -0600

    PWA tweeks

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
